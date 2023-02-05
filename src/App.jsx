33ac156dd09a2f07990c5ca6f38edc9f706e92cd
Valery Pronin <pownmymail@gmail.com>
2023-02-05 15:34:13 +0300

    feat: style main element

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <div className="App">
-     
+    <div className="App flex items-center justify-center min-h-screen">
+      <main className="w-[360px] h-[380px] max-w-full max-h-full bg-slate-900 py-8 px-5">
+        <div className="bg-white w-full h-full rounded-xl">
+          
+        </div>
+      </main>
     </div>
   )
 }
